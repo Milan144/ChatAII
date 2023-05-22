e8f8ea6df85ec37b52e78444c8851f49c31acd7d
Milan <milan.hommet@protonmail.com>
2023-05-22 16:20:11 +0200

    changing client

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,27 +1,59 @@
-import 'package:chatgpt_client/api/chat_api.dart';
-import 'package:chatgpt_client/chat_page.dart';
+import 'api/open_ai_api.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_gpt/utils/cache_service.dart';
+import 'package:flutter_gpt/screens/chat_screen.dart';
+import 'package:flutter_gpt/providers/open_ai_provider.dart';
+import 'package:flutter_gpt/screens/introduction_screen.dart';
+import 'package:flutter_gpt/providers/text_to_speech_provider.dart';
 
 void main() {
-  runApp(ChatApp(chatApi: ChatApi()));
+  runApp(const MyApp());
 }
 
-class ChatApp extends StatelessWidget {
-  const ChatApp({required this.chatApi, super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
-  final ChatApi chatApi;
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String? _apiKey;
+
+  Future<void> _initializeChatScreenRoute() async {
+    final String? fetchedKey =
+        await CacheService.getOpenAIAPIKey(key: "apiKey");
+    setState(() {
+      _apiKey = fetchedKey;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    OpenAIAPI.init();
+    _initializeChatScreenRoute();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ChatGPT Client',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.teal,
-          secondary: Colors.lime,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<OpenAIProvider>(
+          create: (_) => OpenAIProvider(),
         ),
+        ChangeNotifierProvider<TextToSpeechProvider>(
+          create: (_) => TextToSpeechProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter GPT',
+        debugShowCheckedModeBanner: false,
+        home: _apiKey != null
+            ? const ChatScreen()
+            : const CustomIntroductionScreen(),
       ),
-      home: ChatPage(chatApi: chatApi),
     );
   }
 }
