a0231bdc20a540e27afdb75497daabf63e6a905d
Milan <hommetmilan@gmail.com>
2023-06-07 15:33:33 +0200

    Fix routes and database

--- server/app/application.py
+++ server/app/application.py
@@ -1,9 +1,8 @@
 import pymysql
 import os
-import requests
 from dotenv import load_dotenv
 import openai
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 # OpenAI API key
 load_dotenv()
@@ -13,15 +12,15 @@ print("Api key loaded")
 
 application = app = Flask(__name__)
 
-def get_connection():
+
+def create_db_connection():
     connection = pymysql.connect(
         host="mysql-db",
         user="root",
         password="root",
-        db="chataii",
-        port=3306,
+        database="chataii",
         charset="utf8mb4",
-        cursorclass=pymysql.cursors.DictCursor,
+        cursorclass=pymysql.cursors.DictCursor
     )
     return connection
 
@@ -31,277 +30,308 @@ def shorten():
     return "Welcome to Chataii API"
 
 
+# TODO : Implement LOGIN TOKEN
 
 # ? OPENAI API REQUEST
+def openai_request(prompt):
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                              messages=[
+                                                  {"role": "user", "content": prompt}])
+    return completion.choices[0].message.content
 
 
-def openAI(question):
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": question}],
-    )
-    print(completion.choices[0].message.content)
+# ! Users routes
 
-
-#! Users routes
 # Get all users
-app.route("/users", methods=["GET"])
+@app.route("/users", methods=["GET"])
 def getUsers():
-    cursor = get_connection.cursor()
-    cursor.execute("SELECT * FROM user")
-    return jsonify(cursor.fetchall())
-
-
-# # Get one User
-# app.route("/users/<id>", methods=["GET"])
-
-
-# def getUser(id):
-#     cursor = db.cursor()
-#     cursor.execute("SELECT * FROM user WHERE id = %s", (id))
-#     return jsonify(cursor.fetchone())
-
-
-# # Create User
-# app.route("/users", methods=["POST"])
-
-
-# def createUser():
-#     username = requests.json["name"]
-#     password = requests.json["password"]
-#     cursor = db.cursor()
-#     cursor.execute(
-#         "INSERT INTO user (username, password) VALUES (%s, %s)", (username, password)
-#     )
-#     db.commit()
-#     return jsonify({"message": "User created successfully"})
-
-
-# # Update User
-# app.route("/users/<id>", methods=["PUT"])
-
-
-# def updateUser(id):
-#     username = requests.json["name"]
-#     password = requests.json["password"]
-#     cursor = db.cursor()
-#     cursor.execute(
-#         "UPDATE user SET username = %s, password = %s WHERE id = %s",
-#         (username, password, id),
-#     )
-#     db.commit()
-#     return jsonify({"message": "User updated successfully"})
-
-
-# # Login and get token
-# app.route("/auth", methods=["POST"])
-
-
-# def login():
-#     username = requests.json["username"]
-#     password = requests.json["password"]
-#     cursor = db.cursor()
-#     cursor.execute(
-#         "SELECT * FROM user WHERE username = %s AND password = %s", (username, password)
-#     )
-#     user = cursor.fetchone()
-#     if user is not None:
-#         return jsonify({"message": "Login successful", "token": user[3]})
-#     else:
-#         return jsonify({"message": "Login failed"})
-
-
-# #! Universes routes
-# # Get all universes
-# app.route("/universes", methods=["GET"])
-
-
-# def getUniverses():
-#     cursor = db.cursor()
-#     cursor.execute("SELECT * FROM universe")
-#     return jsonify(cursor.fetchall())
-
-
-# # Get one universe
-# app.route("/universes/<id>", methods=["GET"])
-
-
-# def getUniverse(id):
-#     cursor = db.cursor()
-#     cursor.execute("SELECT * FROM universe WHERE id = %s", (id))
-#     return jsonify(cursor.fetchone())
-
-
-# # Create universe
-# app.route("/universes", methods=["POST"])
-
-
-# def createUniverse():
-#     name = requests.json["name"]
-#     cursor = db.cursor()
-#     cursor.execute("INSERT INTO universe (name) VALUES (%s)", (name))
-#     db.commit()
-#     return jsonify({"message": "Universe created successfully"})
-
-
-# # Update universe name
-# app.route("/universes/<id>", methods=["PUT"])
-
-
-# def updateUniverse(id):
-#     name = requests.json["name"]
-#     cursor = db.cursor()
-#     cursor.execute("UPDATE universe SET name = %s WHERE id = %s", (name, id))
-#     db.commit()
-#     return jsonify({"message": "Universe updated successfully"})
-
-
-# #! Characters routes
-# # Get all characters of an universe
-# app.route("/universes/<id>/characters", methods=["GET"])
-
-
-# def getCharacters(id):
-#     cursor = db.cursor()
-#     cursor.execute("SELECT * FROM character WHERE id_universe = %s", (id))
-#     return jsonify(cursor.fetchall())
-
-
-# # Get one character
-# app.route("/universes/<iduniverse>/characters/<idcharacter>", methods=["GET"])
-
-
-# def getCharacter(iduniverse, idcharacter):
-#     cursor = db.cursor()
-#     cursor.execute(
-#         "SELECT * FROM character WHERE id = %s AND id_universe = %s",
-#         (idcharacter, iduniverse),
-#     )
-#     return jsonify(cursor.fetchone())
-
-
-# # Create character
-# app.route("/universes/<id>/characters", methods=["POST"])
-
-
-# def createCharacter(id):
-#     name = requests.json["name"]
-#     universe = getUniverse(id)
-#     description = openAI("Describe " + name + "from " + universe + " in 50 words.")
-#     cursor = db.cursor()
-#     cursor.execute(
-#         "INSERT INTO character (name, description, id_universe) VALUES (%s, %s, %s)",
-#         (name, description, id),
-#     )
-#     db.commit()
-#     return jsonify({"message": "Character created successfully"})
-
-
-# # Generate new description for character
-# app.route("/universes/<iduniverse>/characters/<idcharacter>", methods=["PUT"])
-
-
-# def updateCharacter(iduniverse, idcharacter):
-#     name = requests.json["name"]
-#     universe = getUniverse(iduniverse)
-#     description = openAI("Describe " + name + "from " + universe + " in 50 words.")
-#     cursor = db.cursor()
-#     cursor.execute(
-#         "UPDATE character SET name = %s, description = %s WHERE id = %s AND id_universe = %s",
-#         (name, description, idcharacter, iduniverse),
-#     )
-#     db.commit()
-#     return jsonify({"message": "Character updated successfully"})
-
-
-# #! Conversations routes
-
-# # Get all conversations
-# app.route("/conversations", methods=["GET"])
-
-
-# def getConversations():
-#     cursor = db.cursor()
-#     cursor.execute("SELECT * FROM conversation")
-#     return jsonify(cursor.fetchall())
-
-
-# # Get one conversation
-# app.route("/conversations/<id>", methods=["GET"])
-
-
-# def getConversation(id):
-#     cursor = db.cursor()
-#     cursor.execute("SELECT * FROM conversation WHERE id = %s", (id))
-#     return jsonify(cursor.fetchone())
-
-
-# # Create conversation
-# app.route("/conversations", methods=["POST"])
-
-
-# def createConversation():
-#     idcharacter = requests.json["idcharacter"]
-#     iduser = requests.json["iduser"]
-#     cursor = db.cursor()
-#     cursor.execute(
-#         "INSERT INTO conversation (id_character, id_user) VALUES (%s, %s)",
-#         (idcharacter, iduser),
-#     )
-#     db.commit()
-#     return jsonify({"message": "Conversation created successfully"})
-
-
-# # Delete conversation
-# app.route("/conversations/<id>", methods=["DELETE"])
-
-
-# def deleteConversation(id):
-#     cursor = db.cursor()
-#     cursor.execute("DELETE FROM conversation WHERE id = %s", (id))
-#     db.commit()
-#     return jsonify({"message": "Conversation deleted successfully"})
-
-
-# #! Messages routes
-
-# # Get all messages of a conversation
-# app.route("/conversations/<id>/messages", methods=["GET"])
-
-
-# def getMessages(id):
-#     cursor = db.cursor()
-#     cursor.execute("SELECT * FROM message WHERE id_conversation = %s", (id))
-#     return jsonify(cursor.fetchall())
-
-
-# # Get one message of a conversation
-# app.route("/conversations/<idconv>/messages/<idmsg>", methods=["GET"])
-
-
-# def getMessage(idconv, idmsg):
-#     cursor = db.cursor()
-#     cursor.execute(
-#         "SELECT * FROM message WHERE id = %s AND id_conversation = %s", (idmsg, idconv)
-#     )
-#     return jsonify(cursor.fetchone())
-
-
-# # Send message in a conversation TODO: Send message in a conversation
-# app.route("/conversations/<id>/messages", methods=["POST"])
-
-
-# def sendMessage(id):
-#     message = requests.json["message"]
-#     cursor = db.cursor()
-#     cursor.execute(
-#         "INSERT INTO message (message, id_conversation) VALUES (%s, %s)", (message, id)
-#     )
-#     db.commit()
-#     return jsonify({"message": "Message sent successfully"})
-
-
-# # TODO : Regenerate last message of a conversation
+    connection = create_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM user")
+            result = cursor.fetchall()
+            return jsonify(result)
+    finally:
+        connection.close()
+
+
+# Get one User
+@app.route("/users/<id>", methods=["GET"])
+def getUser(id):
+    connection = create_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM user WHERE id = %s", (id))
+            result = cursor.fetchone()
+            return jsonify(result)
+    finally:
+        connection.close()
+
+
+# Create User
+@app.route("/users", methods=["POST"])
+def createUser():
+    connection = create_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            username = request.json["username"]
+            password = request.json["password"]
+            cursor.execute(
+                "INSERT INTO user (username, password) VALUES (%s, %s)",
+                (username, password),
+            )
+            connection.commit()
+            return jsonify({"message": "User created successfully"})
+    finally:
+        connection.close()
+
+
+# Update User
+@app.route("/users/<id>", methods=["PUT"])
+def updateUser(id):
+    connection = create_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            username = request.json["username"]
+            password = request.json["password"]
+            cursor.execute(
+                "UPDATE user SET username = %s, password = %s WHERE id = %s",
+                (username, password, id),
+            )
+            connection.commit()
+            return jsonify({"message": "User updated successfully"})
+    finally:
+        connection.close()
+
+
+# Login and get token
+@app.route("/auth", methods=["POST"])
+def login():
+    connection = create_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            username = request.json["username"]
+            password = request.json["password"]
+            cursor.execute(
+                "SELECT * FROM user WHERE username = %s AND password = %s",
+                (username, password),
+            )
+            result = cursor.fetchone()
+            if result:
+                return jsonify({"token": result["id"]})
+            else:
+                return jsonify({"message": "Wrong credentials"})
+    finally:
+        connection.close()
+
+
+# ! Universes routes
+# Get all universes
+@app.route("/universes", methods=["GET"])
+def getUniverses():
+    connection = create_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM universe")
+            result = cursor.fetchall()
+            return jsonify(result)
+    finally:
+        connection.close()
+
+
+# Get one universe
+@app.route("/universes/<universeId>", methods=["GET"])
+def getUniverse(universeId):
+    connection = create_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM universe WHERE universeId = %s", universeId)
+            result = cursor.fetchone()
+            return jsonify(result)
+    finally:
+        connection.close()
+
+
+# Create universe
+@app.route("/universes", methods=["POST"])
+def createUniverse():
+    connection = create_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            name = request.json["name"]
+            cursor.execute("INSERT INTO universe (name) VALUES (%s)", (name))
+            connection.commit()
+            return jsonify({"message": "Universe created successfully"})
+    finally:
+        connection.close()
+
+
+# Update universe name
+@app.route("/universes/<universeId>", methods=["PUT"])
+def updateUniverse(universeId):
+    connection = create_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            name = request.json["name"]
+            cursor.execute("UPDATE universe SET name = %s WHERE universeId = %s", (name, universeId))
+            connection.commit()
+            return jsonify({"message": "Universe updated successfully"})
+    finally:
+        connection.close()
+
+
+# ! Characters routes
+# Get all characters of a universe
+@app.route("/universes/<id>/characters", methods=["GET"])
+def getCharacters(id):
+    connection = create_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM characterai WHERE universeId = %s", (id))
+            result = cursor.fetchall()
+            return jsonify(result)
+    finally:
+        connection.close()
+
+
+# Get one character
+@app.route("/universes/<iduniverse>/characters/<idcharacter>", methods=["GET"])
+def getCharacter(iduniverse, idcharacter):
+    connection = create_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT * FROM characterai WHERE universeId = %s AND id = %s",
+                (iduniverse, idcharacter),
+            )
+            result = cursor.fetchone()
+            return jsonify(result)
+    finally:
+        connection.close()
+
+
+# Create character
+@app.route("/universes/<universeId>/characters", methods=["POST"])
+def createCharacter(universeId):
+    connection = create_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            name = request.json["name"]
+            universe = getUniverse(universeId).json["name"]
+
+            prompt = "Describe the story of " + name + " from " + universe + " in 100 words max."
+            history = openai_request(prompt)
+
+            cursor.execute(
+                "INSERT INTO `characterai` (name, history, universeId) VALUES (%s, %s, %s)",
+                (name, history, universeId),
+            )
+            connection.commit()
+            return jsonify({"message": "Character created successfully"})
+    finally:
+        connection.close()
+
+
+# Generate new description for character
+@app.route("/universes/<iduniverse>/characters/<idcharacter>", methods=["PUT"])
+def updateCharacter(iduniverse, idcharacter):
+    connection = create_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            name = request.json["name"]
+            universe = getUniverse(iduniverse)
+            history = openai_request("Describe " + name + "from " + universe + " in 50 words.")
+            cursor.execute(
+                "UPDATE characterai SET name = %s, history = %s WHERE id = %s",
+                (name, history, idcharacter),
+            )
+            connection.commit()
+            return jsonify({"message": "Character updated successfully"})
+    finally:
+        connection.close()
+
+
+# ! Conversations routes
+
+# Get all conversations
+@app.route("/conversations", methods=["GET"])
+def getConversations():
+    connection = create_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM conversation")
+            result = cursor.fetchall()
+            return jsonify(result)
+    finally:
+        connection.close()
+
+
+# Get one conversation
+@app.route("/conversations/<conversationId>", methods=["GET"])
+def getConversation(conversationId):
+    connection = create_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM conversation WHERE conversationId = %s", (conversationId))
+            result = cursor.fetchone()
+            return jsonify(result)
+    finally:
+        connection.close()
+
+
+# Create conversation
+@app.route("/conversations", methods=["POST"])
+def createConversation():
+    connection = create_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            userId = request.json["userId"]
+            characterId = request.json["characterId"]
+            cursor.execute(
+                "INSERT INTO conversation (userId, characterId) VALUES (%s, %s)",
+                (userId, characterId),
+            )
+            connection.commit()
+            return jsonify({"message": "Conversation created successfully"})
+    finally:
+        connection.close()
+
+
+# ! Messages routes
+
+# Get all messages of a conversation
+@app.route("/conversations/<id>/messages", methods=["GET"])
+def getMessages(id):
+    connection = create_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM message WHERE id_conversation = %s", (id))
+            result = cursor.fetchall()
+            return jsonify(result)
+    finally:
+        connection.close()
+
+
+# Get one message of a conversation
+@app.route("/conversations/<idconv>/messages/<idmsg>", methods=["GET"])
+def getMessage(idconv, idmsg):
+    connection = create_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT * FROM message WHERE id_conversation = %s AND id = %s",
+                (idconv, idmsg),
+            )
+            result = cursor.fetchone()
+            return jsonify(result)
+    finally:
+        connection.close()
+
+
+# TODO: Send message in a conversation
+
+
+# TODO : Regenerate last message of a conversation
 
 
 if __name__ == "__main__":
---
+++ server/initdb/Schema.sql
@@ -0,0 +1,187 @@
+CREATE DATABASE chataii;
+USE chataii;
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `character`
+--
+
+CREATE TABLE `characterai`
+(
+    characterId INT NOT NULL,
+    name        VARCHAR(255),
+    history     VARCHAR(6000),
+    universeId  int DEFAULT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `conversation`
+--
+
+CREATE TABLE `conversation`
+(
+    `conversationId` int(11) NOT NULL,
+    `characterId`    int(11)      DEFAULT NULL,
+    `userId`         varchar(255) DEFAULT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `message`
+--
+
+CREATE TABLE `message`
+(
+    `messageId`      int(11) NOT NULL,
+    `content`        varchar(255) DEFAULT NULL,
+    `conversationId` int(11)      DEFAULT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `universe`
+--
+
+CREATE TABLE `universe`
+(
+    `universeId` int(11) NOT NULL,
+    `name`       varchar(255) DEFAULT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user`
+(
+    userId     INT          NOT NULL,
+    `username` varchar(255) NOT NULL,
+    `password` varchar(255) DEFAULT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `character`
+--
+ALTER TABLE `characterai`
+    ADD PRIMARY KEY (`characterId`),
+    ADD KEY `universeId` (`universeId`);
+
+--
+-- Index pour la table `conversation`
+--
+ALTER TABLE `conversation`
+    ADD PRIMARY KEY (`conversationId`),
+    ADD KEY `characterId` (`characterId`),
+    ADD KEY `userId` (`userId`);
+
+--
+-- Index pour la table `message`
+--
+ALTER TABLE `message`
+    ADD PRIMARY KEY (`messageId`),
+    ADD KEY `conversationId` (`conversationId`);
+
+--
+-- Index pour la table `universe`
+--
+ALTER TABLE `universe`
+    ADD PRIMARY KEY (`universeId`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+    ADD PRIMARY KEY (`userId`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `character`
+--
+ALTER TABLE `characterai`
+    MODIFY `characterId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `conversation`
+--
+ALTER TABLE `conversation`
+    MODIFY `conversationId` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `user`
+    MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `message`
+--
+ALTER TABLE `message`
+    MODIFY `messageId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `universe`
+--
+ALTER TABLE `universe`
+    MODIFY `universeId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `character`
+--
+ALTER TABLE `characterai`
+    ADD CONSTRAINT `character_ibfk_1` FOREIGN KEY (`universeId`) REFERENCES `universe` (`universeId`);
+
+--
+-- Contraintes pour la table `conversation`
+--
+ALTER TABLE `conversation`
+    ADD CONSTRAINT `conversation_ibfk_1` FOREIGN KEY (`characterId`) REFERENCES `characterai` (`characterId`),
+    ADD CONSTRAINT `conversation_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `user` (`username`);
+
+--
+-- Contraintes pour la table `message`
+--
+ALTER TABLE `message`
+    ADD CONSTRAINT `message_ibfk_1` FOREIGN KEY (`conversationId`) REFERENCES `conversation` (`conversationId`);
+COMMIT;
+
+
+-- Creating test data
+
+-- Insert test data into the `universe` table
+INSERT INTO `universe` (`universeId`, `name`)
+VALUES (1, 'League of Legends');
+
+-- Insert test data into the `characterai` table
+INSERT INTO `characterai` (`characterId`, `name`, `history`, `universeId`)
+VALUES (1, 'Gangplank', 'A pirate from bilgewater', 1);
+
+-- Insert test data into the `conversation` table
+INSERT INTO `conversation` (`conversationId`, `characterId`, `userId`)
+VALUES (1, 1, 1);
+
+-- Insert test data into the `message` table
+INSERT INTO `message` (`messageId`, `content`, `conversationId`)
+VALUES (1, 'Test message', 1);
+
+-- Insert test data into the `user` table
+INSERT INTO `user` (`userId`, `username`, `password`)
+VALUES (1, 'test', 'test');
